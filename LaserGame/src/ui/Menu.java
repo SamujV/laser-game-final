da2cd4a17b6e9e1f6a3c4b7aae3d1a68776806fd
SamujV <samuelviviescas@hotmail.com>
2020-12-11 18:10:48 -0500

    method showMenu added

--- LaserGame/src/ui/Menu.java
+++ LaserGame/src/ui/Menu.java
@@ -13,9 +13,33 @@ public class Menu {
 	public void start() {
 		int option;
 		option = Integer.parseInt(sc.nextLine());
+		manageMenu(option);
+		if(option != 3) {
+			start();
+		}		
+	}	
+	
+	public void manageMenu(int o) {
+		switch(o) {
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
 		
+		default:
+		}
 	}
 	
+	
+	public void showMenu() {
+		String msg = "\n";
+		msg += " 1) Play. \n";
+		msg += " 2) Laderboard. \n";
+		msg += " 3) Exit.\n";
+		System.out.println(msg);
+	}
 	
 	
 	
