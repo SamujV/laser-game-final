350bef5d126e353d375e09531a5a45350565117e
SamujV <samuelviviescas@hotmail.com>
2020-12-11 18:07:49 -0500

    method manageMenu added

--- LaserGame/src/ui/Menu.java
+++ LaserGame/src/ui/Menu.java
@@ -1,13 +1,18 @@
 package ui;
 
+import java.util.Scanner;
+
 public class Menu {
 	
+	private Scanner sc = new Scanner(System.in);
 	
 	public Menu() {
 		
 	}
 	
 	public void start() {
+		int option;
+		option = Integer.parseInt(sc.nextLine());
 		
 	}
 	
