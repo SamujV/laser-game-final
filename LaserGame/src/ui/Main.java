c7d6c8957f294e7686e4143003567d467d63e2f9
SamujV <samuelviviescas@hotmail.com>
2020-12-11 17:40:57 -0500

    main added

---
+++ LaserGame/src/ui/Main.java
@@ -0,0 +1,13 @@
+package ui;
+
+import model.LinkedMatrix;
+
+public class Main {
+
+	public static void main(String[] args) {
+		LinkedMatrix lm = new LinkedMatrix(3,4);
+		System.out.println(lm);		
+	}
+
+
+}
